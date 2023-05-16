d63e58bf2a07996d719ab78115ceac9d83b73f14
email <allentusar909@gmail.com>
2023-05-16 12:07:42 +0600

    get specific data for specific appointment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.port || 3000;
@@ -31,6 +31,13 @@ async function run() {
     app.get("/doctors", async (req, res) => {
       const cursor = doctorCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/doctors/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await doctorCollection.findOne(query);
       res.send(result);
     });
 
